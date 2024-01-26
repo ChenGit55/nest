b8fcb2e8fc24a717688808a84985a6d7c2c04a09
ChenGit55 <chenpsilva@gmail.com>
2024-01-26 20:34:06 -0300

    Enviroment variables created

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,11 +6,11 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: 'postgres',
-        database: 'DesafioDB',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
         synchronize: false,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       });
